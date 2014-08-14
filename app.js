fedf3aebf5253074413daf8230c5b47f02d22146
Nick <nikitam851@gmail.com>
2014-08-14 15:24:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**
  * Created by Arkkienkeli on 14.08.2014.
  */
-var text = document.getElementById("listelem").value; 
 function  addElement(text) {
     var list = document.getElementById('list');
     var li = document.createElement('LI');
