0e1bc47eb349e200b9d193f97495841ab6c8cb4f
Nick <nikitam851@gmail.com>
2014-08-15 05:37:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 /**
  * Created by Arkkienkeli on 14.08.2014.
  */
-function  addElement(text) {
+function  addTask(text) {
+
     var list = document.getElementById('list');
     var li = document.createElement('LI');
     li.innerHTML = text;
-    list.appendChild(li)
+
+    var delBtn = document.createElement('input');
+    delBtn.type = "button";
+    delBtn.value = "Удалить элемент";
+    delBtn.onclick = function() { delTask(this.parentNode); };
+
+    var checkBox = document.createElement('input');
+    checkBox.type = "checkbox";
+    checkBox.id = "checkBox";
+    checkBox.onclick = function() { selectTask(this) ; };
+
+    list.appendChild(li);
+    li.appendChild(delBtn);
+    li.appendChild(checkBox);
+}
+
+function delTask(node) {
+    node.parentNode.removeChild(node);
+}
+
+function selectTask(checkbox) {
+    if (checkbox.checked) {
+        checkbox.parentNode.style.color = 'red';
+    }
+    else {
+        checkbox.parentNode.style.color = 'black';
+    }
 }
